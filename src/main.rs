f88823c9e2753c15fe3462b519dd80a5799cb5f4
mariano <mariano.gonzalez.mx@gmail.com>
2023-03-01 18:43:23 -0600

    Add implementation for args and commands

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
+use structopt::StructOpt;
+
 mod args;
 mod chunk;
 mod chunk_type;
 mod commands;
 mod png;
-
-
-use structopt::StructOpt;
 
 pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
